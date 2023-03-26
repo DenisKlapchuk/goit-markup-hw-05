2f8b45606d90250c8a7bd7778cc8e0662b26c3ad
DenisKlapchuk <113906500+DenisKlapchuk@users.noreply.github.com>
2023-03-26 22:13:34 +0300

    hw5

--- modal.js
+++ modal.js
@@ -2,7 +2,7 @@
     const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[ ]"),
+    modal: document.querySelector("[data-modal]"),
     };
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
